9cf462c36d235cdf9d36d95c5a431b78f5f8e4f5
dianadias <murphy2027@hotmail.com>
2017-11-12 15:17:39 -0200

    Customização do site pronta para uso no tema da Start Bootstrap.

--- js/my.js
+++ js/my.js
@@ -1,52 +1,21 @@
-popup = {
+popupPortifolioVideo = {
       init: function(){
-        $('figure').click(function(){ //CHAMA A FUNCAO OPEN PARA PARA EXIBIR A IMG ESCOLHIDA
-          popup.open($(this));
-        });
-
-        $(document).on('click', '.popup', function(){ //FECHA A IMG QUANDO CLICA NO X PARA FECHAR A IMAGEM ESCOLHIDA
-          popup.close();
-        }).keyup(function(e) { // FECHA A MODAL CASO DE ESC
-        if (e.keyCode == 27) {
-        // alert('escape');
-        $('#portfolioModal2').hide();
-        $('.modal-backdrop').hide();
-        }
-      }).on('click', '.modal-backdrop', function(){ //FECHA A MODAL CASO CLICK FORA DAS IMAGENS
-        setTimeout(function(){
-          $('.modal-backdrop').hide();
-          $('#portfolioModal2').hide();
-          $('.modal-open').css('overflow-y', 'auto'); //COLOCA A BARRA DE ROLAGEM
-        }, 100);
-      })
-      },
-      //FUNCAO PARA EXIBIR A IMAGEM ESCOLHIDA
-      open: function($figure) {
-        $('.gallery').addClass('pop');
-        $popup = $('<div class="popup" />').appendTo($('#portfolioModal2'));
-        $fig = $figure.clone().appendTo($('.popup'));
-        $bg = $('<div class="bg" />').appendTo($('.popup'));
-        $close = $('<div class="close"><svg><use xlink:href="#close"></use></svg></div>').appendTo($fig);
-        $shadow = $('<div class="shadow" />').appendTo($fig);
-        src = $('img', $fig).attr('src');
-        $shadow.css({backgroundImage: 'url(' + src + ')'});
-        $bg.css({backgroundImage: 'url(' + src + ')'});
-      setTimeout(function(){
-        $('.popup').addClass('pop');
-      }, 10);
-      },
-      //FUNCAO PARA FECHAR A IMAGEM ESCOLHIDA
-      close: function(){
-        $('.gallery, .popup').removeClass('pop');
-        setTimeout(function(){
-          $('.popup').remove();
-        }, 100);
-      }
-    }
+      $('.close-modal').on('click', function (e) {
+  // caso clique me fechar a modal no X...
+      $('#video').attr("src", $("#video").attr("src"));
+      $('#video1').attr("src", $("#video1").attr("src"));
+      $('#video2').attr("src", $("#video2").attr("src"));
+  });
+      $('.fecha-video').on('click', function (e) {
+  // Caso clique em fechar videos btn amarelo...
+      $('#video').attr("src", $("#video").attr("src"));
+      $('#video1').attr("src", $("#video1").attr("src"));
+      $('#video2').attr("src", $("#video2").attr("src"));
+  });
 
     //QUANDO DER ESC HABILLITA AS DIVS PARA MOSTRAR AS FOTOS NOVAMENTE
-    $('.portfolio-hover').on('click', function() {
-    $('#portfolioModal2').show();
-    $('.modal-backdrop').show();
+    // $('.portfolio-hover').on('click', function() {
+    // $('#portfolioModal2').show();
+    // $('.modal-backdrop').show();
     // popup.init();
-  });
+  }};
